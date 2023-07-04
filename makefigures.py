4f139fff4929963c18effab9168581c204a35089
Cristian <gcd2006@gmail.com>
2023-07-04 13:22:04 +0300

    A

--- makefigures.py
+++ makefigures.py
@@ -11,7 +11,7 @@ from plotly.subplots import make_subplots
 #################################
 
 def make_charts(df, ticker):
-    fig = make_subplots(rows=4, cols=1, vertical_spacing=0.005, shared_xaxes=True, row_heights=[0.6, 0.1,0.1,0.6])
+    fig = make_subplots(rows=4, cols=1, vertical_spacing=0.005, subplot_titles=(ticker,"Stochastic Oscillator","RSI","Kelner channels"), shared_xaxes=True, row_heights=[0.6, 0.1,0.1,0.6])
 
     fig.add_trace(go.Candlestick(x=df['Date'],
                 open=df['Open'],
@@ -57,8 +57,6 @@ def make_charts(df, ticker):
 
     fig.update_layout(height=1600) 
 
-    fig.update_layout(
-        title_text=ticker)
 
     fig.update_layout(
         xaxis=dict(
